d4adcb3811a46838f4181ea37222651d9ac230ea
Sergio Heredia <sergioheredia1996@gmail.com>
2023-08-25 02:22:09 -0700

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -10,9 +10,9 @@ console.log(user);
 function App() {
   return (
     <div>
-      <NavBar />
-      <Home />
-      <About />
+      <NavBar/>
+      <Home color={user.color} name={user.name} city={user.city}/>
+      <About bio={user.bio} github={user.links.github} linkedin={user.links.linkedin}/>
     </div>
   );
 }
